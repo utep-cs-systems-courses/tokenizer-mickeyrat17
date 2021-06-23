2804427e2dedc344aa9a9d3b1917499699c19ff0
Daniel Moreno <dcmoreno17@gmail.com>
2021-06-22 19:08:45 -0600

    Added implementation for space_char and non_space_char methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,18 +1,19 @@
 #include "tokenizer.h"
 
-/* Return true (non-zero) if c is a whitespace characer
-   ('\t' or ' ').  
-   Zero terminators are not printable (therefore false) */
-int space_char(char c);
+// checks if char is a space or tab
+int space_char(char c)
+{
+  if(c==' ' || c == '\t') return 1;
+  return 0;
+}
 
-/* Return true (non-zero) if c is a non-whitespace 
-   character (not tab or space).  
-   Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+//returns true if char is not a space or zero-terminator
+int non_space_char(char c)
+{
+  if(space_char(c) || c =='\0') return 0;
+  return 1;
+}
 
-/* Returns a pointer to the first character of the next 
-   space-separated word in zero-terminated str.  Return a zero pointer if 
-   str does not contain any words. */
 char word_start(char *str); 
 
 /* Returns a pointer terminator char following *word */
